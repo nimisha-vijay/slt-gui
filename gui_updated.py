de1d566c0ec0e1c712ba1c9a29de92f9e01d5a56
nimisha-vijay <nimisha.k.vijay@gmail.com>
2021-04-09 23:25:16 +0530

    added help screens, it's pretty much done now

--- gui_updated.py
+++ gui_updated.py
@@ -2,38 +2,35 @@ import PySimpleGUI as sg
 import cv2
 import numpy as np
 import time
-import asyncio
 
-
-
-async def main():
-    
+def main():
     
     sg.theme('Black')
 
     # define the window layout
-    startScreen = [
-                [sg.Text('Sign Language Translator', size=(40, 1), justification='left', font='Helvetica 20')],
-                [sg.Text('Point the camera towards the signer and press record', size=(40, 1), justification='left', font='Helvetica 14')],
-                [sg.Button('Start', key="START", size=(8, 1), font='Helvetica 14'),]
+    startScreen =   [
+                        [sg.Text('Sign Language Translator', size=(40, 1), justification='left', font='Helvetica 20')],
+                        [sg.Text('Point the camera towards the signer and press record', size=(40, 2), justification='left', font='Helvetica 14')],
+                        [sg.Button('Start', key="START", size=(8, 1), font='Helvetica 14'),]
                     ]
     
     appScreen = [
-              [sg.Image(filename='', key='image')],
-              [sg.Button('Record', key="RECORD", size=(8, 1), font='Helvetica 14'),
-               sg.Button('Stop', size=(8, 1), font='Helvetica 14'), ]
-               ]
+                    [sg.Image(filename='', key='image')],
+                    [   sg.Button('Record', key="RECORD", size=(10, 1), font='Helvetica 14'),
+                        sg.Button('Stop', key="STOP", size=(8, 1), visible=False, font='Helvetica 14'),
+                        sg.Button('Help', key="HELP", size=(8, 1), font='Helvetica 14'), ]
+                ]
                
     messageScreen = [
-                        [sg.Text('Nice to meet you', size=(40, 1), justification='left', font='Helvetica 14')],
+                        [sg.Text('Nice to meet you', size=(40, 2), justification='left', font='Helvetica 20')],
                         [sg.Button('Back', key="BACK", size=(8, 1), font='Helvetica 14'),]
                     ]
     layout = [
-                [sg.Column(startScreen, visible=True, key="STARTSCREEN"), 
-                sg.Column(appScreen, visible=False, key="APPSCREEN"),
-                sg.Column(messageScreen, visible=False, key="MSGSCREEN"),]]
+                [   sg.Column(startScreen, visible=True, key="STARTSCREEN"), 
+                    sg.Column(appScreen, visible=False, key="APPSCREEN"),
+                    sg.Column(messageScreen, visible=False, key="MSGSCREEN"),]]
 
-    window = sg.Window('OpenCV Integration',
+    window = sg.Window('Sign Language Translator',
                        layout, location=(0, 0),
                        no_titlebar=True,
                        size=(480,320))
@@ -51,34 +48,53 @@ async def main():
         elif event == "START":
             window["STARTSCREEN"].update(visible=False)
             window["APPSCREEN"].update(visible=True)
+            window["MSGSCREEN"].update(visible=False)
+            window["START"].update(text="Back")
+        
+        elif event == "BACK":
+            window["STARTSCREEN"].update(visible=False)
+            window["APPSCREEN"].update(visible=True)
+            window["MSGSCREEN"].update(visible=False)
+            
+        elif event == "HELP":
+            window["STARTSCREEN"].update(visible=True)
+            window["APPSCREEN"].update(visible=False)
+            window["MSGSCREEN"].update(visible=False)
 
         elif event == "RECORD":
             recording = True 
 
-        elif event == 'Stop':
+        elif event == 'STOP':
             recording = False
               
 
         ret, frame = cap.read()
         # cv2.waitKey(1)
-        img = frame[0:270, 0:480]
+        img = frame[0:240, 0:480]
         imgbytes = cv2.imencode('.png', img)[1].tobytes()  # ditto
         window['image'].update(data=imgbytes)
         
         if recording:
             video.append([frame])
             cnt += 1
-            window["RECORD"].update(text="Recording", button_color="#FFFF00")
+            window["RECORD"].update(text="Recording... " + str(int(cnt/7) + 1), button_color="#FFFF00")
+            window["STOP"].update(visible=True)
+            window["HELP"].update(visible=False)
         else:
             cnt = 0
             window["RECORD"].update(text="Record", button_color="#FFFFFF") 
+            window["STOP"].update(visible=False)
+            window["HELP"].update(visible=True)
         
         if cnt == 20:
             # processVideo(video)
             video = []
             recording = False
             cnt = 0
+            window["STARTSCREEN"].update(visible=False)
+            window["APPSCREEN"].update(visible=False)
+            window["MSGSCREEN"].update(visible=True)
         print(cnt, round(time.time()*1000))
 
 
-asyncio.run(main())
+main()
